c987bac88ad42ea000285bc2346504f0ec0268dd
peerapat <peerapat2902549@gmail.com>
2026-01-09 18:08:50 +0700

    lab13_1.cpp completed

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -19,3 +19,27 @@ int main(){
     cout << "Min = " << B[5];
     return 0;
 }
+void stat(const double A[], int N, double B[]){
+    double sum = 0;
+    double sumSq = 0;
+    double dot = 1;
+    double harmonic = 0;
+    double max = A[0];
+    double min = A[0];
+    for(int i = 0; i < N; i++){
+        sum += A[i];
+        sumSq += A[i] * A[i];
+        dot = dot * A[i];
+        harmonic += 1 / A[i];
+        if(A[i] > max) 
+            max = A[i];
+        if(A[i] < min) 
+            min = A[i];
+    }
+    B[0] = sum / N;
+    B[1] = sqrt((sumSq / N) - (B[0] * B[0]));
+    B[2] = pow(dot, 1.0 / N);
+    B[3] = N / harmonic;
+    B[4] = max;
+    B[5] = min;
+}
